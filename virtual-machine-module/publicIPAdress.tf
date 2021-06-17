b27cd73fec78521e8a6716b684d091cf65e8ae54
Saurabh <saurabh.d2106@gmail.com>
2021-06-17 11:43:43 +0530

    update vm1

--- virtual-machine-module/publicIPAdress.tf
+++ virtual-machine-module/publicIPAdress.tf
@@ -1,6 +1,6 @@
 resource "azurerm_public_ip" "myterraformpublicip" {
   name                = var.ipaddressName
-  location            = "eastus"
+  location            = var.location
   resource_group_name = azurerm_resource_group.myterraformgroup.name
   allocation_method   = "Dynamic"
 
